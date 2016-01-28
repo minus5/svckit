2921da5dd928b603701d74e1d0ffe3db59e52b3b
Igor Anic <igor.anic@gmail.com>
2016-01-28 17:06:30 +0100

    Isplati listic u bazi i pkg/db
    Migracija se nalazi u web_backend projektu. U pkg/db je poziv i
    testovi. U cmd/dbi sam otvorio interface.

--- pkg/msgs/listici.go
+++ pkg/msgs/listici.go
@@ -13,3 +13,24 @@ type DokumentiDopuna struct {
 	Offset     int64  `json:"offset"`
 	Limit      int64  `json:"limit"`
 }
+
+type IsplatiListicReq struct {
+	IgracId string `json:"igrac_id"`
+	Broj    string `json:"broj"`
+	Kod     string `json:"kod"`
+}
+
+type IsplatiListicRsp struct {
+	Status      int
+	Raspolozivo float64
+	Dobitak     float64
+	Listic      map[string]interface{}
+}
+
+const (
+	IsplatiListicStatusOk = iota
+	IsplatiListicStatusIgracNijePronadjen
+	IsplatiListicStatusNijePronadjen
+	IsplatiListicStatusNijeDobitan
+	IsplatiListicStatusIsplacen
+)
