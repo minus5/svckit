3966e978e76b829cf697ac375670d033d398817b
Igor Anic <igor.anic@gmail.com>
2016-01-29 10:55:26 +0100

    Refactoring isplati_listici -> listici/isplati
    Trenutno mi se cini da je ovo bolji naziv. Kada poslije dodje
    listici/dodaj lijepo ce sjesti.

---
+++ pkg/msgs/listici_isplati.go
@@ -0,0 +1,29 @@
+package msgs
+
+import "encoding/json"
+
+const (
+	ListiciIsplatiStatusOk = iota
+	ListiciIsplatiStatusIgracNijePronadjen
+	ListiciIsplatiStatusNijePronadjen
+	ListiciIsplatiStatusNijeDobitan
+	ListiciIsplatiStatusIsplacen
+)
+
+type ListiciIsplatiReq struct {
+	IgracId string `json:"igrac_id"`
+	Broj    string `json:"broj"`
+	Kod     string `json:"kod"`
+}
+
+type ListiciIsplatiRsp struct {
+	Status      int
+	Raspolozivo float64
+	Dobitak     float64
+	Listic      map[string]interface{}
+}
+
+func (req *ListiciIsplatiReq) ToJson() []byte {
+	buf, _ := json.Marshal(req)
+	return buf
+}
