097766612143a213d71bb3a34ffdf78d2f444dbd
jsladovic <janko.sladovic@minus5.hr>
2016-02-11 11:48:38 +0100

    Vraceno dohvacanje sportradar statistike
    Vraceno dohvacanje Sportradar statisitke, StatistikaId preimenovano u
    SportradarId, dodan lock na dogadjaj tijekom dohvacanja te export.
    Ispravljeni testovi koji su failali zbog exporta lige u index.

---
+++ pkg/msgs/sportradar.go
@@ -0,0 +1,51 @@
+package msgs
+
+import "encoding/json"
+
+type SportradarIdRequest struct {
+	Id      int    `json:"id"`
+	Izvor   int    `json:"izvor"`
+	IzvorId int    `json:"izvorId"`
+	BaseId  int    `json:"baseId"`
+	Status  string `json:"status"`
+}
+
+type SportradarIdResponse struct {
+	SportradarId     int `json:"statistikaId"`
+	SportradarLigaId int `json:"statistikaLigaId"`
+}
+
+func (r SportradarIdRequest) ToJson() []byte {
+	buf, _ := json.Marshal(r)
+	return buf
+}
+
+func (r SportradarIdResponse) ToJson() []byte {
+	buf, _ := json.Marshal(r)
+	return buf
+}
+
+func SportradarRspFromJson(b []byte) (*SportradarIdResponse, error) {
+	rsp := &SportradarIdResponse{}
+	err := json.Unmarshal(b, rsp)
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+func SportradarReqFromJson(b []byte) (*SportradarIdRequest, error) {
+	req := &SportradarIdRequest{}
+	err := json.Unmarshal(b, req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+const (
+	SportradarStatusNepoznat  = "nepoznat"
+	SportradarStatusNovi      = "novi dogadjaj"
+	SportradarStatusAktiviran = "dogadjaj aktiviran"
+	SportradarStatusBaseId    = "postavljen base ID"
+)
