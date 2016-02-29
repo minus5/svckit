5d75915e2ca7e42f5ed2c70ce7e211fad9db5718
vedran <vedran.vekic@minus5.hr>
2016-02-29 11:49:15 +0100

    Definirani defaultni portovi za servise
    https://basecamp.com/1771540/projects/9855103/todos/237674260
    
    Umjesto da su posijani doslovni portovi u svakom servisu, sad su
    definirani kao konstante u pkg/defaults.

---
+++ pkg/defaults/port.go
@@ -0,0 +1,21 @@
+package defaults
+
+const (
+	PortBackendApi         = "8220"
+	PortBetgeniusConnector = "80"
+	PortBetradarConnector  = "8120"
+	PortBetradarMerger     = "8130"
+	PortDbToNsq            = "8160"
+	PortDbi                = "8091"
+	PortDevCentrala        = "8081"
+	PortHttpToNsq          = "8110"
+	PortPushNot            = "8170"
+	PortNsqToHttp          = "8180"
+	PortNsqToMongo         = "8190"
+	PortNsqToSse           = "8100"
+	PortTecajna            = "8090"
+	PortTecajnaPprof       = "8092"
+	PortVsoccer            = "8080"
+	PortWebAppApi          = "8080"
+	PortNsqToWs            = "8210"
+)
