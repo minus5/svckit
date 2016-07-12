8196ab37bfb9b02de2fcc23e0acca4eee2416818
gljubojevic <goran.ljubojevic@minus5.hr>
2016-07-12 14:28:06 +0200

    Dodati mogucnost da se klijenti prijave na Firebase push notifikacije
    U poruku za prijavu na push notifikacije dodaj ne fcm_id koji sluzi kao
    id za Firebase klijenta.

--- pkg/msgs/push_not_subscribe.go
+++ pkg/msgs/push_not_subscribe.go
@@ -1,12 +1,21 @@
 package msgs
 
+// Poruka za prijavu na push notifikacije
 type PushNotSubscribe struct {
+	// remember_token igraca koji se prijavljuje za notifikacije
 	IgracId string `json:"igrac_id"`
+	// Uredjaj s kojim se prijavljuje za notifikacije
 	Uredjaj struct {
+		// Google Cloud Messaging device id 
 		GcmId   string `json:"gcm_id"`
+		// Firebase Cloud Messaging device id 
+		FcmId   string `json:"fcm_id"`
+		// Apple Cloud Messaging device id 
 		AppleId string `json:"apple_id"`
+		// Da li je uredjaj aktivan, ako je false koristi se za deaktivaciju uredjaja
 		Aktivan bool   `json:"aktivan"`
 	} `json:"uredjaj"`
+	// Notifikacije na koje se igrac pretplacuje
 	Pretplate struct {
 		PrivatnePoruke  bool `json:"privatne_poruke"`
 		Novosti         bool `json:"novosti"`
