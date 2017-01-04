130067b690dc848c616db55d2eb9f82ef5fe9c87
Igor Anic <igor.anic@gmail.com>
2017-01-04 13:55:33 +0100

    Uljepsati json dokumenti dopuna
    Dodati omitempty na json atribute

--- pkg/msgs/dokumenti_dopuna.go
+++ pkg/msgs/dokumenti_dopuna.go
@@ -3,8 +3,12 @@ package msgs
 type DokumentiDopuna struct {
 	IgracId    string `json:"igrac_id"`
 	Tip        string `json:"tip"`
-	Count      int64  `json:"count"`
-	MongoCount int64  `json:"mongo_count"`
-	Offset     int64  `json:"offset"`
-	Limit      int64  `json:"limit"`
+	Count      int64  `json:"count,omitempty"`
+	MongoCount int64  `json:"mongo_count,omitempty"`
+	Offset     int64  `json:"offset,omitempty"`
+	Limit      int64  `json:"limit,omitempty"`
+}
+
+func (dd DokumentiDopuna) TipIgraci() bool {
+	return dd.Tip == "igraci"
 }
