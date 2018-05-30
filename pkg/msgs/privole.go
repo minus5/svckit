8507fbd63c50d35d279942cf5e288102c03f5e6a
djelusic <djelusic133@gmail.com>
2018-05-30 11:06:53 +0200

    Spremiti IP igraca uz profile evente

--- pkg/msgs/privole.go
+++ pkg/msgs/privole.go
@@ -1,16 +1,19 @@
 package msgs
 
 type UvjetiKoristenjaReq struct {
-	IgracId string `json:"igrac_id"`
-	Verzija int    `json:"verzija"`
+	IgracId  string `json:"igrac_id"`
+	Verzija  int    `json:"verzija"`
+	RemoteIP string `json:"remote_ip"`
 }
 
 type NewsletterPostavkeReq struct {
-	IgracId string `json:"igrac_id"`
-	Status  int    `json:"status"`
+	IgracId  string `json:"igrac_id"`
+	Status   int    `json:"status"`
+	RemoteIP string `json:"remote_ip"`
 }
 
 type SMSPostavkeReq struct {
-	IgracId string `json:"igrac_id"`
-	Status  int    `json:"status"`
+	IgracId  string `json:"igrac_id"`
+	Status   int    `json:"status"`
+	RemoteIP string `json:"remote_ip"`
 }
