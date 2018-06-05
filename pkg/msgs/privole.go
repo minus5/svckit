a5248fee8b924113656a41ee6390d80368be2b37
djelusic <djelusic133@gmail.com>
2018-06-05 10:37:53 +0200

    Dodati novi endpoint za postavke privatnosti

--- pkg/msgs/privole.go
+++ pkg/msgs/privole.go
@@ -17,3 +17,10 @@ type SMSPostavkeReq struct {
 	Status   int    `json:"status"`
 	RemoteIP string `json:"remote_ip"`
 }
+
+type PostavkePrivatnostiReq struct {
+	IgracId          string `json:"igrac_id"`
+	NewsletterStatus int    `json:"newsletter_status"`
+	SMSStatus        int    `json:"sms_status"`
+	RemoteIP         string `json:"remote_ip"`
+}
