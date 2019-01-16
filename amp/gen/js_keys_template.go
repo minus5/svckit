4c2b08fcc32bb0a38c6088fe681c1cddacf3f540
Igor Anic <ianic@inet.hr>
2019-01-16 15:38:57 +0100

    amp/gen: modify generated javascritp unpack code
    Making it more readable, shorter, pure js compatabile.

--- amp/gen/js_keys_template.go
+++ amp/gen/js_keys_template.go
@@ -3,41 +3,43 @@ package gen
 import "text/template"
 
 var jsKeysTemplate = template.Must(template.New("").Funcs(fns).Parse(`
-  function unpack{{.Type}}(o) {
-  {{- range .Fields }}
+  function _unpack{{.Type}}(o) {
+    var keys = {
+    {{- range .Fields }}
+      {{- if .NeedUnpack }}
+      "{{.TagName}}": "{{.NameLower}}",
+    {{- end}}
+    {{- end}}
+    {{- range .Maps}}
+    {{- if .NeedUnpack }}
+      "{{.TagName}}": "{{.FieldLower}}",
+    {{- end}}
+    {{- end}}
+
+    {{- range .Structs}}
     {{- if .NeedUnpack }}
     if (o["{{.TagName}}"] !== undefined) {
-      o["{{.NameLower}}"] = o["{{.TagName}}"];
-      delete o["{{.TagName}}"];
-     }
-    {{- end}}
-  {{- end}}
-  {{- range .Maps}}
-    {{- if .NeedUnpack }}
-    if (o["{{.TagName}}"] != undefined) {
-      o["{{.FieldLower}}"] = o["{{.TagName}}"];
-      delete o["{{.TagName}}"];
-     }
-    {{- end}}
-     for(var k in o["{{.FieldLower}}"]) {
-       var c = o["{{.FieldLower}}"][k];
-       if (c !== null) {
-         unpack{{.Value}}(c);
-       }
-     }
-  {{- end}}
-  {{- range .Structs}}
-    {{- if .NeedUnpack }}
-    if (o["{{.TagName}}"] !== undefined) {
-      o["{{.FieldLower}}"] = o["{{.TagName}}"];
-      delete o["{{.TagName}}"];
+      "{{.TagName}}": "{{.FieldLower}}",
     }
     {{- end}}
+    {{- end}}
+    };
+    _unpack(o, keys);
+
+    {{- range .Maps}}
+    for(var k in o["{{.FieldLower}}"]) {
+      var c = o["{{.FieldLower}}"][k];
+      if (c !== null) {
+        _unpack{{.Value}}(c);
+      }
+    }
+    {{- end}}
+    {{- range .Structs}}
     var s = o["{{.FieldLower}}"];
     if (s !== undefined && Object.keys(s).length > 0) {
       s["_isStruct"] = 1;
-      unpack{{.Field}}(s);
+      _unpack{{.Field}}(s);
     }
-  {{- end}}
+    {{- end}}
   }
 `))
