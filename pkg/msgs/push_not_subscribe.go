0f48da2f3ae623fea0e51b71b2bfc4314b18fa45
jpop <jpop32@gmail.com>
2020-10-02 14:39:43 +0200

    dodati ParVrednovan polje u PushNotSubscribe

--- pkg/msgs/push_not_subscribe.go
+++ pkg/msgs/push_not_subscribe.go
@@ -13,21 +13,22 @@ type PushNotSubscribe struct {
 	IgracId string `json:"igrac_id"`
 	// Uredjaj s kojim se prijavljuje za notifikacije
 	Uredjaj struct {
-		// Google Cloud Messaging device id 
-		GcmId		string	`json:"gcm_id"`
-		// Firebase Cloud Messaging device id 
-		FcmId		string	`json:"fcm_id"`
-		// Apple Cloud Messaging device id 
-		AppleId		string	`json:"apple_id"`
+		// Google Cloud Messaging device id
+		GcmId string `json:"gcm_id"`
+		// Firebase Cloud Messaging device id
+		FcmId string `json:"fcm_id"`
+		// Apple Cloud Messaging device id
+		AppleId string `json:"apple_id"`
 		// Da li je uredjaj aktivan, ako je false koristi se za deaktivaciju uredjaja
-		Aktivan		bool	`json:"aktivan"`
+		Aktivan bool `json:"aktivan"`
 		// Tip uredjaja za primanje push notifikacija, 0 - Android, 1 - iOS, 2 - web
-		DeviceType	int		`json:"device_type"`
+		DeviceType int `json:"device_type"`
 	} `json:"uredjaj"`
 	// Notifikacije na koje se igrac pretplacuje
 	Pretplate struct {
 		PrivatnePoruke  bool `json:"privatne_poruke"`
 		Novosti         bool `json:"novosti"`
 		ListicVrednovan bool `json:"listic_vrednovan"`
+		ParVrednovan    bool `json:"par_vrednovan"`
 	} `json:"pretplate"`
 }
